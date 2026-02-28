ef7c31e1c8a7f620406f2fdca244a03c1e7896dc
Aung Thu Hein <agthuhein.mm@gmail.com>
2026-02-28 21:27:50 +0100

    user login

--- edenthought/journal/views.py
+++ edenthought/journal/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm
 
 
 def homepage(request):
     return render(request, "journal/index.html")
 
 
+# User Registration
 def register(request):
-    return render(request, "journal/register.html")
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+
+    context = {"RegistrationForm": form}
+    return render(request, "journal/register.html", context)
 
 
 def my_login(request):
