c629cc3e910eabe5c02dc5b073def68e77cb9e42
Aung Thu Hein <agthuhein.mm@gmail.com>
2026-02-28 17:35:39 +0100

    update settings for database

--- edenthought/edenthought/settings.py
+++ edenthought/edenthought/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'journal',
+    "journal",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "edenthought.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "journal",
+        "USER": "root",
+        "PASSWORD": "root12345",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 
@@ -116,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
